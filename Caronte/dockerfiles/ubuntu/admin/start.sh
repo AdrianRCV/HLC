0a7ec525fc7de2a730b8e1d07a3d379ce054faac
AdrianRCV <adriancamposvargas4@gmail.com>
2024-11-21 09:10:22 +0000

    20241121

--- Caronte/dockerfiles/ubuntu/admin/start.sh
+++ Caronte/dockerfiles/ubuntu/admin/start.sh
@@ -2,11 +2,14 @@
 set -e
 
 source ./usuarios/usuarios.sh
+surce ./ssh/ssh.sh
 
 chmod +x /root/admin/usuarios/usuarios.sh
+chmod +x /
 
 main(){
    newUser  
+   config_ssh
    # make_bienvenida
 
     tail -f /dev/null 
